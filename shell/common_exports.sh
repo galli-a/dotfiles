a48e7f282eb01951048f39b2ed28f5480035467e
galli-a <andrea.gallivanoni@gmail.com>
2016-09-24 09:40:40 +0200

    updated Python package location to Dropbox

--- shell/common_exports.sh
+++ shell/common_exports.sh
@@ -8,7 +8,7 @@ export PP3DATA="/usr/local/share/pp3/"
 export MANPATH="/Users/andrea/Applications/testdisk:/Users/andrea/Applications/pdfjam/man1:$MANPATH"
 
 #export PYTHONPATH=/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python:$PYTHONPATH
-export PYTHONUSERBASE=$HOME/Python_modules
+export PYTHONUSERBASE=$HOME/Dropbox/Python_modules
 #export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 export PYTHONPATH=$PYTHONUSERBASE:$PYTHONUSERBASE/lib/python/site-packages:$PYTHONPATH
 export PYTHONSTARTUP=$HOME/.pythonrc
